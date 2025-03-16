89ec6e05bd30920df3330cefad473eed2356ac24
Ricajust <ricartoonstudio@gmail.com>
2025-03-16 10:24:30 -0300

    feat: adicionando a validacao de campos e a regra de cpf

--- edugo_tcc/src/main/java/com/edugo/edugo_tcc/model/Usuario.java
+++ edugo_tcc/src/main/java/com/edugo/edugo_tcc/model/Usuario.java
@@ -2,7 +2,8 @@ package com.edugo.edugo_tcc.model;
 
 import java.time.LocalDate;
 import java.util.UUID;
-
+import javax.validation.constraints.*;
+import com.edugo.edugo_tcc.validation.CPFValido;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,17 +11,43 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
-    public UUID id;
-    public String nome;
-    public String cpf;
-    public LocalDate dataNascimento;
-    public String email;
-    public String telefone;
-    public String endereco;
-    public String bairro;
-    public String cidade;
-    public String uf;
-    public String cep;
-    public String senha;
+public abstract class Usuario {
+
+    @NotNull
+    private UUID id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String nome;
+
+    @CPFValido
+    private String cpf;
+
+    @Past
+    private LocalDate dataNascimento;
+
+    @Email
+    private String email;
+
+    @Size(min = 8, max = 20)
+    private String telefone;
+
+    @Size(max = 255)
+    private String endereco;
+
+    @Size(max = 100)
+    private String bairro;
+
+    @Size(max = 100)
+    private String cidade;
+
+    @Size(min = 2, max = 2)
+    private String uf;
+
+    @Size(min = 8, max = 9)
+    private String cep;
+
+    @NotBlank
+    @Size(min = 8)
+    private String senha;
 }
