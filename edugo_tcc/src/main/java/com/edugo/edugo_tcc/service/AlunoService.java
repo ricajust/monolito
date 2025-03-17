049b8a51f56dd558ab54f386033a8f6bdfd748b2
Ricajust <ricartoonstudio@gmail.com>
2025-03-16 22:21:59 -0300

    feat: criacao da alunoService

---
+++ edugo_tcc/src/main/java/com/edugo/edugo_tcc/service/AlunoService.java
@@ -0,0 +1,14 @@
+package com.edugo.edugo_tcc.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.edugo.edugo_tcc.dto.AlunoDTO;
+
+public interface AlunoService {
+    AlunoDTO criarAluno(AlunoDTO alunoDTO);
+    AlunoDTO buscarAlunoPorId(UUID id);
+    List<AlunoDTO> buscarTodosAlunos();
+    AlunoDTO atualizarAluno(UUID id, AlunoDTO alunoDTO);
+    AlunoDTO excluirAluno(UUID id);
+}
