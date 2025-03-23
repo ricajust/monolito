4c2bde144e47bac202cafcd644f811415403f382
Ricajust <ricartoonstudio@gmail.com>
2025-03-23 07:00:54 -0300

    feat: configuracao inicial do rabbitmq

--- edugo_tcc/src/main/java/com/edugo/edugo_tcc/config/RabbitMQConfig.java
+++ edugo_tcc/src/main/java/com/edugo/edugo_tcc/config/RabbitMQConfig.java
@@ -1,5 +1,14 @@
 package com.edugo.edugo_tcc.config;
 
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
 
+    @Bean
+    public FanoutExchange alunoExchange() {
+        return new FanoutExchange("aluno-exchange");
+    }
 }
